9846df79194902f2f34cbe8e8fcc5b21cab39589
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2024-03-19 14:46:48 +0000

    primiera questão feita

---
+++ mat.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+    int a[4][8];
+    int i,j;
+    int soma = 0;
+    for (i = 0; i<4; i++){
+        for(j=0; j<8; j++){
+            scanf("%i", &a[i][j]);
+        }
+    }
+    for (i = 0; i<4; i++){
+        printf("A soma da linha %i é: ", i+1);
+        for(j=0; j<8; j++){
+            soma += a[i][j];
+            if(j == 7){
+                if(soma%2 == 0){
+                    printf("%i, e a soma é par.\n", soma);
+                    soma = 0;
+                }
+                else if(soma%2 != 0){
+                    printf("%i, e a soma é impar.\n", soma);
+                    soma = 0;
+                }
+            }
+        }
+    }
+    printf("\n");
+    for (i = 0; i<8; i++){
+        printf("A soma da coluna %i é: ", i+1);
+        for(j=0; j<4; j++){
+            soma += a[j][i];
+            if(j == 3){
+                if(soma%2 == 0){
+                    printf("%i, e a soma é par.\n", soma);
+                    soma = 0;
+                }
+                else if(soma%2 != 0){
+                    printf("%i, e a soma é impar.\n", soma);
+                    soma = 0;
+                }
+            }
+        }
+    }
+}
