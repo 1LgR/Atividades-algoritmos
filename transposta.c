978ed6a44ffd4558cf507e0b6364d0f57a434163
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2024-03-19 15:11:10 +0000

    questão transposta feita

---
+++ transposta.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int l,c;
+    printf("Digite quantas linhas e colunas você quer que a matriz tenha: ");
+    scanf("%i%i", &l,&c);
+    int a[l][c];
+    int i,j;
+    for (i = 0; i<l; i++){
+        for(j=0; j<c; j++){
+            scanf("%i", &a[i][j]);
+        }
+    }
+    for (i = 0; i<c; i++){
+        for(j=0; j<l; j++){
+            printf("%i\t", a[j][i]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
